ce4fc4488e1dbbf6f304c7bd192b54938ea2bf3d
ravi750321 <ravistotalgaming@gmail.com>
2025-05-04 07:57:18 +0000

    changes Done

--- inventoryManage.php
+++ inventoryManage.php
@@ -16,9 +16,29 @@
 <body>
   <div class="grid">
 
-    <div id="navbar" class="item"><span>
+    <div id="navbar" class="item">
+
+      <span>
         <h4>Shop Management</h4>
       </span>
+
+      <div class="dropdown text-end">
+
+        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+          <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+        </a>
+
+        <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+          <li><a class="dropdown-item" href="#">New project...</a></li>
+          <li><a class="dropdown-item" href="#">Settings</a></li>
+          <li><a class="dropdown-item" href="#">Profile</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="#">Sign out</a></li>
+        </ul>
+
+      </div>
 
     </div>
 
@@ -27,9 +47,7 @@
         <h4>Inventory</h4>
 
         <ul>
-
           <li><a href="./dashboard.php">Dashboard</a></li>
-
           <li class="mb-1">
             <button
               class="btn-li"
@@ -72,7 +90,21 @@
     </div>
 
     <div id="section">
-      
+
+      <header class="p-3 mb-3 border-bottom">
+        <div class="container">
+          
+          <h4>Inventory Manage</h4>
+
+          <!-- <form>
+            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+          </form> -->
+
+          <a href="./addNewItem.php" class="btn btn-warning">+Add New Item</a>
+          
+        </div>
+      </header>
+
     </div>
     <div id="footer" class="item">@Copyright Shop Management</div>
   </div>
