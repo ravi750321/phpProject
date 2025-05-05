a29d92c2fb13e1fb8081cca53903a2cbf81b3816
ravi750321 <ravistotalgaming@gmail.com>
2025-05-05 04:25:12 +0000

    Done Till Grid

--- homepage.php
+++ homepage.php
@@ -4,99 +4,96 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>User Login</title>
+  <title>Home Page</title>
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="./css/grid.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./css/sidenav.css">
+  <link rel="stylesheet" href="./css/grid.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
   <div class="grid">
 
-    <div id="navbar" class="item"><span>
-        <h4>Shop Management</h4>
+    <div id="navbar" class="item">
+      <div class="flex-nav">
+        <span>
+          <h4>Shop Management</h4>
+        </span>
+
+        <div class="dropdown text-end">
+
+          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+
+          <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div id="aside" class="item">
+      <?php
+       include("./sidenav.html");
+      ?>
+    </div>
+
+    <div id="section" class="item">
+      <div class="header border-bottom">
+
+        <h4>Dashboard</h4>
+
+                    <!-- <form>
+                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+                    </form> -->
+      </div>
+
+      <div class="overflow-auto">
+        <div class="row">
+          <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+              <h5 class="card-header">Featured</h5>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <h5 class="card-header">Featured</h5>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div id="footer" class="item">
+
+      <span>
+        @Copyright Shop Management
       </span>
-
     </div>
 
-    <div id="aside">
-      <div class="sidebar-nav">
-        <h4>Inventory</h4>
-
-        <ul>
-
-          <li><a href="./dashboard.php">Dashboard</a></li>
-
-          <li class="mb-1">
-            <button
-              class="btn-li"
-              data-bs-toggle="collapse"
-              data-bs-target="#inventory-collapse"
-              aria-expanded="true">
-              Inventory
-            </button>
-            <div class="collapse show" id="inventory-collapse">
-              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a href="./inventoryItem.php">Items</a></li>
-                <li><a href="#">Inventory Adjustments</a></li>
-              </ul>
-            </div>
-          </li>
-
-          <li class="mb-1">
-            <button
-              class="btn-li"
-              data-bs-toggle="collapse"
-              data-bs-target="#sales-collapse"
-              aria-expanded="false">
-              Sales Management
-            </button>
-            <div class="collapse" id="sales-collapse">
-              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a href="#">Customers</a></li>
-                <li><a href="./invoices.php">Invoices</a></li>
-                <li><a href="#">Payment Recieved</a></li>
-                <li><a href="#">Sales Return</a></li>
-              </ul>
-            </div>
-          </li>
-          <li><a href="./purchase.php">Purchase</a></li>
-          <li><a href="./employee.php ">Employee Management</a></li>
-          <li><a href="#">Reports</a></li>
-        </ul>
-      </div>
-
-    </div>
-
-    <div id="section">
-      <div class="row">
-        <div class="col-sm-6 mb-3 mb-sm-0">
-          <div class="card">
-          <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card">
-          <h5 class="card-header">Featured</h5>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div id="footer" class="item">@Copyright Shop Management</div>
   </div>
+
 </body>
 
 </html>
--- inventoryManage.php
+++ inventoryManage.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>User Login</title>
+  <title>Inventory Manage</title>
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="./css/gridTable.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./css/sidenav.css">
+  <link rel="stylesheet" href="./css/gridTable.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 
 </head>
 
@@ -17,160 +17,129 @@
   <div class="grid">
 
     <div id="navbar" class="item">
+      <div class="flex-nav">
+        <span>
+          <h4>Shop Management</h4>
+        </span>
 
-      <span>
-        <h4>Shop Management</h4>
-      </span>
+        <div class="dropdown text-end">
 
-      <div class="dropdown text-end">
-         
-        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-        </a>
+          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
 
-        <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-          <li><a class="dropdown-item" href="#">Profile</a></li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-          <li><a class="dropdown-item" href="#">Sign out</a></li>
-        </ul>
-
-      </div>
-
-    </div>
-
-    <div id="aside">
-      <div class="sidebar-nav">
-        <h4>Inventory</h4>
-
-        <ul>
-          <li><a href="./dashboard.php">Dashboard</a></li>
-          <li class="mb-1">
-            <button
-              class="btn-li"
-              data-bs-toggle="collapse"
-              data-bs-target="#inventory-collapse"
-              aria-expanded="true">
-              Inventory
-            </button>
-            <div class="collapse show" id="inventory-collapse">
-              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a href="./inventoryItem.php">Items</a></li>
-                <li><a href="#">Inventory Adjustments</a></li>
-              </ul>
-            </div>
-          </li>
-
-          <li class="mb-1">
-            <button
-              class="btn-li"
-              data-bs-toggle="collapse"
-              data-bs-target="#sales-collapse"
-              aria-expanded="false">
-              Sales Management
-            </button>
-            <div class="collapse" id="sales-collapse">
-              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a href="#">Customers</a></li>
-                <li><a href="./invoices.php">Invoices</a></li>
-                <li><a href="#">Payment Recieved</a></li>
-                <li><a href="#">Sales Return</a></li>
-              </ul>
-            </div>
-          </li>
-          <li><a href="./purchase.php">Purchase</a></li>
-          <li><a href="./employee.php ">Employee Management</a></li>
-          <li><a href="#">Reports</a></li>
-        </ul>
-      </div>
-
-    </div>
-
-    <div id="section">
-
-      <header class="p-3 mb-3 border-bottom">
-        <div class="container">
-
-          <h4>Inventory Manage</h4>
-
-          <!-- <form>
-            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-          </form> -->
-
-          <a href="./addNewItem.php" class="btn btn-warning">+Add New Item</a>
+          <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
 
         </div>
-      </header>
 
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Shop_id</th>
-            <th>Name</th>
-            <th>SKU</th>
-            <th>Description</th>
-            <th>Stock In Hand</th>
-            <th>Unit</th>
-            <th>Rate / Unit</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
+      </div>
 
-          <tr>
-            <td>Default</td>
-            <td>Defaultson</td>
-            <td>def@somemail.com</td>
-            <td>Default</td>
-            <td>Defaultson</td>
-            <td>def@somemail.com</td>
-            <td>Default</td>
-            <td>
-              <a href="./editItem.php?item_id=" class="btn btn-primary">Edit</a>
-
-              <a href="./inventoryItem.php?item_id=" class="btn btn-danger">Delete</a>
-            </td>
-          </tr>
-
-          <tr>
-            <td>Default</td>
-            <td>Defaultson</td>
-            <td>def@somemail.com</td>
-            <td>Default</td>
-            <td>Defaultson</td>
-            <td>def@somemail.com</td>
-            <td>Default</td>
-            <td>
-              <a href="./editItem.php?item_id=" class="btn btn-primary">Edit</a>
-
-              <a href="./inventoryItem.php?item_id=" class="btn btn-danger">Delete</a>
-            </td>
-          </tr>
-
-          <tr>
-            <td>Default</td>
-            <td>Defaultson</td>
-            <td>def@somemail.com</td>
-            <td>Default</td>
-            <td>Defaultson</td>
-            <td>def@somemail.com</td>
-            <td>Default</td>
-            <td>
-              <a href="./editItem.php?item_id=" class="btn btn-primary">Edit</a>
-
-              <a href="./inventoryItem.php?item_id=" class="btn btn-danger">Delete</a>
-            </td>
-          </tr>
-
-          <tr class="table-danger">
-            <td colspan="8">No Record Found</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
-    <div id="footer" class="item">@Copyright Shop Management</div>
+
+    <div id="aside" class="item">
+      <?php
+      include("./sidenav.html");
+      ?>
+    </div>
+
+    <div id="section" class="item">
+      <div class="header border-bottom">
+
+        <h4>Inventory Manage</h4>
+
+        <!-- <form>
+                    <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+                    </form> -->
+
+        <a href="./addNewItem.php" class="btn btn-warning">+Add New Item</a>
+
+      </div>
+
+      <div class="overflow-auto">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Shop_id</th>
+              <th>Name</th>
+              <th>SKU</th>
+              <th>Description</th>
+              <th>Stock In Hand</th>
+              <th>Unit</th>
+              <th>Rate / Unit</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <tr>
+              <td>Default</td>
+              <td>Defaultson</td>
+              <td>def@somemail.com</td>
+              <td>Default</td>
+              <td>Defaultson</td>
+              <td>def@somemail.com</td>
+              <td>Default</td>
+              <td>
+                <a href="./editItem.php?item_id=" class="btn btn-primary">Edit</a>
+
+                <a href="./inventoryItem.php?item_id=" class="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Default</td>
+              <td>Defaultson</td>
+              <td>def@somemail.com</td>
+              <td>Default</td>
+              <td>Defaultson</td>
+              <td>def@somemail.com</td>
+              <td>Default</td>
+              <td>
+                <a href="./editItem.php?item_id=" class="btn btn-primary">Edit</a>
+
+                <a href="./inventoryItem.php?item_id=" class="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Default</td>
+              <td>Defaultson</td>
+              <td>def@somemail.com</td>
+              <td>Default</td>
+              <td>Defaultson</td>
+              <td>def@somemail.com</td>
+              <td>Default</td>
+              <td>
+                <a href="./editItem.php?item_id=" class="btn btn-primary">Edit</a>
+
+                <a href="./inventoryItem.php?item_id=" class="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+
+            <tr class="table-danger">
+              <td colspan="8">No Record Found</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+
+    <div id="footer" class="item">
+
+      <span>
+        @Copyright Shop Management
+      </span>
+    </div>
+
   </div>
+
 </body>
 
 </html>
