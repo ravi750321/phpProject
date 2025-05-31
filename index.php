eaa6947ea2b2bb9cb7541a712ac2b341e3736f13
ravi750321 <ravistotalgaming@gmail.com>
2025-05-31 10:23:38 +0000

    Changes Done

--- index.php
+++ index.php
@@ -1,44 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>User Login</title>
-  
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-<link rel="stylesheet" href="./css/headerFooter.css">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+  <style>
+    .card {
+      width: 33%;
+      margin: auto;
+      margin-top: 5rem;
+      box-shadow: 0.1rem 0.1rem 0.1rem #949497;
+    }
+
+    form {
+      padding: 0.5rem 1rem;
+    }
+
+    form h4 {
+      text-align: center;
+    }
+  </style>
 </head>
+
 <body>
-  <div class="grid">
+  <div class="card">
 
-    <div id="navbar" class="item"><span><h4>Shop Management</h4></span>
-    <span class="nav-bar">Don't Have a Account?<a class="nav-link" href="./register.php" style="display: inline;color:#fff">Register</a>
-        </span>
-    </div>
 
-    <div id="section">
-      <div class="login-box" style="margin:1rem 3rem;">
-      <form id="loinForm" action="./index.php" method="post">
-          <h2>User Login</h2>
 
-          <div class="mb-3">
-            <label for="username" class="form-label">Username</label>
-            <input type="text" id="username" class="form-control" name="username" required>
-          </div>
-
-          <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password">
-          </div>
-         
-        <div class="links">
-          <button type="submit" class="btn btn-primary">Login</button>
-          <p style="margin-top: 0.6rem;">If you are a new?<a href="./register.php">Register a shop</a></p>
-        </div>
-        </form>
+    <form id="loinForm" action="./index.php" method="post">
+      <h4>Login</h4>
+      <div class="mb-3">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" id="username" class="form-control" name="username" placeholder="Enter Username" required>
       </div>
-    </div>
-  <div id="footer" class="item">@Copyright Shop Management</div>
+
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Enter Password" required>
+      </div>
+
+    </form>
+          <button style="margin: 0rem 1rem;" type="submit" class="btn btn-primary">Login</button>
+          <p style="margin-top: 0.6rem; text-align:center;">If you are a new?<a href="./register.php"> Register a shop</a></p>
   </div>
 </body>
 
--- register.php
+++ register.php
@@ -1,69 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Shop Registration</title>
-  
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-<link rel="stylesheet" href="./css/headerFooter.css">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+
+  <style>
+    .card {
+      width: 35%;
+      margin: auto;
+      margin-top: 1rem;
+      box-shadow: 0.1rem 0.1rem 0.1rem #949497;
+    }
+
+    form {
+      padding: 0.2rem 1rem;
+    }
+
+    form h3 {
+      text-align: center;
+    }
+  </style>
 
 </head>
+
 <body>
-  <div class="grid">
+  <div class="card">
+    <form id="registerForm" action="./register.php" method="post">
 
-    <div id="navbar" class="item"><span><h4>Shop Management</h4></span>
-    <span class="nav-bar">Already Registered?<a class="nav-link" href="./index.php" style="display: inline;color:#fff;">login</a>
-        </span>
+    <h3>Shop Registration Form</h3>
+    <div class="mb-3">
+      <label for="username" class="form-label">Username</label>
+      <input type="text" id="username" name="username" class="form-control" placeholder="Enter Username" required>
     </div>
 
-    <div id="section" style="height: auto;">
-      <div class="login-box" style="margin:1rem 3rem;">
-      <div class="register-container">
-    <div class="register-box">
-     
-      <h2>Shop Registration Form</h2>
-
-      <form id="registerForm" action="./register.php" method="post">
-
-        <div class="mb-3">
-          <label for="username" class="form-label">Username</label>
-          <input type="text" id="username" name="username" class="form-control" required>
-        </div>
-
-        <div class="mb-3">
-          <label for="shopName"  class="form-label">Shop Name</label>
-          <input type="text" id="shopName" name="shopName" class="form-control" required>
-        </div>
-
-        <div class="mb-3">
-          <label for="address" class="form-label">Address</label>
-          <input type="text" id="address" name="address" class="form-control" required>
-        </div>
-
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input type="password" id="password" name="password" class="form-control" required>
-        </div>
-
-        <div class="mb-3">
-          <label for="confirmPass" class="form-label">Confirm Password</label>
-          <input type="password" id="confirmPass" name="confirmPass" class="form-control" required>
-        </div>
-
-        <button class="btn btn-primary" type="submit" style="margin-bottom: 1rem;">Register</button>
-      </form>
-
-      <div class="links">
-        <p>Already have an account? <a href="./index.php">Login</a></p>
-      </div>
-
+    <div class="mb-3">
+      <label for="shopName" class="form-label">Shop Name</label>
+      <input type="text" id="shopName" name="shopName" class="form-control" placeholder="Enter Shop Name" required>
     </div>
+
+    <div class="mb-3">
+      <label for="address" class="form-label">Address</label>
+      <input type="text" id="address" name="address" class="form-control" placeholder="Enter Address" required>
+    </div>
+
+    <div class="mb-3">
+      <label for="password" class="form-label">Password</label>
+      <input type="password" id="password" name="password" class="form-control" placeholder="Enter Password" required>
+    </div>
+
+    <div class="mb-3">
+      <label for="confirmPass" class="form-label">Confirm Password</label>
+      <input type="password" id="confirmPass" name="confirmPass" class="form-control" placeholder="Repeat Password" required>
+    </div>
+  </form>
+   <button class="btn btn-primary" style="margin: 0rem 1rem;" type="submit" style="margin-bottom: 1rem;">Register</button>
+    <p style="margin-top: 0.6rem; text-align:center;">Already have a account?<a href="./index.php"> Login</a></p>
   </div>
-      </div>
-    </div>
-  <div id="footer" class="item">@Copyright Shop Management</div>
-  </div>
+  
 </body>
 
 </html>
